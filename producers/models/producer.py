4ec2562e471ca66826913f67c6d9c47b16eac931
Dharlan Oliveira <dharlanoliveira@gmail.com>
2020-03-14 12:50:35 -0300

    Evoluindo o projeto

--- producers/models/producer.py
+++ producers/models/producer.py
@@ -31,16 +31,10 @@ class Producer:
         self.num_partitions = num_partitions
         self.num_replicas = num_replicas
 
-        #
-        #
-        # TODO: Configure the broker properties below. Make sure to reference the project README
-        # and use the Host URL for Kafka and Schema Registry!
-        #
-        #
         self.broker_properties = {
-            # TODO
-            # TODO
-            # TODO
+            'bootstrap.servers': 'localhost:9092',
+            'schema.registry.url': 'http://localhost:8081',
+            'on_delivery': self.delivery_report
         }
 
         # If the topic does not already exist, try to create it
@@ -48,32 +42,26 @@ class Producer:
             self.create_topic()
             Producer.existing_topics.add(self.topic_name)
 
-        # TODO: Configure the AvroProducer
-        # self.producer = AvroProducer(
-        # )
+        self.producer = AvroProducer(
+            self.broker_properties,
+            default_key_schema=key_schema, default_value_schema=value_schema
+         )
+
+    def delivery_report(err, msg):
+        if err is not None:
+            print('Message delivery failed: {}'.format(err))
+        else:
+            print('Message delivered to {} [{}]'.format(msg.topic(), msg.partition()))     
 
     def create_topic(self):
-        """Creates the producer topic if it does not already exist"""
-        #
-        #
-        # TODO: Write code that creates the topic for this producer if it does not already exist on
-        # the Kafka Broker.
-        #
-        #
-        logger.info("topic creation kafka integration incomplete - skipping")
+        logger.info(f'Creating topic {self.topic_name}')
+        admin_client = KafkaAdminClient(bootstrap_servers=self.broker_properties['bootstrap.servers'], client_id=f'producer{self.topic_name}!')
+        admin_client.create_topics(new_topics=[NewTopic(name=self.topic_name, num_partitions=self.num_replicas, replication_factor=self.num_replicas)], validate_only=False)
+        logger.info(f'Topic {self.topic_name} created')
 
     def time_millis(self):
         return int(round(time.time() * 1000))
 
     def close(self):
-        """Prepares the producer for exit by cleaning up the producer"""
-        #
-        #
-        # TODO: Write cleanup code for the Producer here
-        #
-        #
-        logger.info("producer close incomplete - skipping")
-
-    def time_millis(self):
-        """Use this function to get the key for Kafka Events"""
-        return int(round(time.time() * 1000))
+        self.producer.close()
+        logger.info("producer close complete")
--- producers/models/station.py
+++ producers/models/station.py
@@ -15,11 +15,7 @@ class Station(Producer):
     """Defines a single station"""
 
     key_schema = avro.load(f"{Path(__file__).parents[0]}/schemas/arrival_key.json")
-
-    #
-    # TODO: Define this value schema in `schemas/station_value.json, then uncomment the below
-    #
-    #value_schema = avro.load(f"{Path(__file__).parents[0]}/schemas/arrival_value.json")
+    value_schema = avro.load(f"{Path(__file__).parents[0]}/schemas/arrival_value.json")
 
     def __init__(self, station_id, name, color, direction_a=None, direction_b=None):
         self.name = name
@@ -31,23 +27,18 @@ class Station(Producer):
             .replace("'", "")
         )
 
-        #
-        #
-        # TODO: Complete the below by deciding on a topic name, number of partitions, and number of
-        # replicas
-        #
-        #
-        topic_name = f"{station_name}" # TODO: Come up with a better topic name
+        topic_name = f"{station_name}"
         super().__init__(
             topic_name,
             key_schema=Station.key_schema,
-            # TODO: value_schema=Station.value_schema, # TODO: Uncomment once schema is defined
-            # TODO: num_partitions=???,
-            # TODO: num_replicas=???,
+            value_schema=Station.value_schema,
+            num_partitions=3,
+            num_replicas=1,
         )
 
         self.station_id = int(station_id)
         self.color = color
+        self.station_name = station_name
         self.dir_a = direction_a
         self.dir_b = direction_b
         self.a_train = None
@@ -57,23 +48,21 @@ class Station(Producer):
 
     def run(self, train, direction, prev_station_id, prev_direction):
         """Simulates train arrivals at this station"""
-        #
-        #
-        # TODO: Complete this function by producing an arrival message to Kafka
-        #
-        #
-        logger.info("arrival kafka integration incomplete - skipping")
-        #self.producer.produce(
-        #    topic=self.topic_name,
-        #    key={"timestamp": self.time_millis()},
-        #    value={
-        #        #
-        #        #
-        #        # TODO: Configure this
-        #        #
-        #        #
-        #    },
-        #)
+        
+        logger.info("Producing arrival")
+        self.producer.produce(
+            topic=self.topic_name,
+            key={"timestamp": self.time_millis()},
+            value={
+                "station_id" : self.station_id,
+                "train_id" :train,
+                "direction" : direction,
+                "line" : self.color,
+                "train_status" : self.direction.train_status,
+                "prev_station_id" : prev_station_id,
+                "prev_direction" : prev_direction, 
+            }
+        )
 
     def __str__(self):
         return "Station | {:^5} | {:<30} | Direction A: | {:^5} | departing to {:<30} | Direction B: | {:^5} | departing to {:<30} | ".format(
